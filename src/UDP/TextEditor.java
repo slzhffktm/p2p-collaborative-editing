dbbbdc5d9700307acd4d541ef25f559f0a5a1563
slzhffktm <13516121@std.stei.itb.ac.id>
2019-04-24 23:59:32 +0700

    add text editor in udp

---
+++ src/UDP/TextEditor.java
@@ -0,0 +1,65 @@
+package UDP;
+
+import javax.swing.*;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.OceanTheme;
+
+public class TextEditor extends JFrame {
+    private JTextArea t;
+    private JFrame f;
+
+    TextEditor() {
+        // Create a frame
+        f = new JFrame("editor");
+
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            MetalLookAndFeel.setCurrentTheme(new OceanTheme());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Text component
+        t = new JTextArea("`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./ |~!@#$%^&*()_+{}:\"<>?", 20,40);
+        t.setLineWrap(true);
+
+        f.add(t);
+        f.pack();
+        f.setVisible(true);
+
+        t.setText("");
+    }
+
+    int getCursorPosition() {
+        return t.getCaretPosition();
+    }
+
+    void setCursorPosition(int position) {
+        if (position <= t.getText().length()) {
+            t.setCaretPosition(position);
+        } else {
+            t.setCaretPosition(t.getText().length());
+        }
+    }
+
+    String getText() {
+        return t.getText();
+    }
+
+    void setText(String text) {
+        t.setText(text);
+    }
+
+    public JTextArea getT() {
+        return t;
+    }
+
+    public void setT(JTextArea t) {
+        this.t = t;
+    }
+
+    public static void main(String[] args) {
+        new TextEditor();
+    }
+}
